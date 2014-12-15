0cd47da7a831e19399cc627bcc16133965c64c2f
Frank J Castro <frankjtc@gmail.com>
2014-12-14 17:59:42 -0800

    first commit

---
+++ WebAPI_WeightConversion/App_Start/WebApiConfig.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Web.Http;
+
+namespace WebAPI_WeightConversion
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //route to convert from Lb to Kg
+            config.Routes.MapHttpRoute(
+              name: "ConvertToKiloApi",
+              routeTemplate: "api/conversion/poundtokilo/{pounds}",
+              defaults: new { controller = "WeightConversion", action = "ConvertLbToKilo" }
+          );
+
+            //route to convert from Kg to Lb
+            config.Routes.MapHttpRoute(
+              name: "ConvertToPoundApi",
+              routeTemplate: "api/conversion/kilotopound/{kilos}",
+              defaults: new { controller = "WeightConversion", action = "ConvertKiloToLb" }
+          );
+
+            //To get the response as Json
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+    }
+}
---
+++ WebAPI_WeightConversion/Controllers/WeightConversionController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPI_WeightConversion.Controllers
+{
+    public class WeightConversionController : ApiController
+    {
+        const double factor = 2.2046226218;
+        //Pound-Kilo Conversion
+        [HttpGet]
+        public IHttpActionResult ConvertLbToKilo(double pounds)
+        {
+            var kilos = pounds /factor;
+            return Ok(kilos);
+        }
+
+        //Kilo-Pound Conversion
+        [HttpGet]
+        public IHttpActionResult ConvertKiloToLb(double kilos)
+        {
+            var pounds = kilos * factor;
+            return Ok(pounds);
+        }
+    }
+}
